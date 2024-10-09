06f4d8419ff42dc8ff1ef0dd45e5309a87cc2f59
Steven C <8864661+scuc@users.noreply.github.com>
2024-10-09 13:14:23 -0400

    chore: update of the filename for the create_db module

--- create_db.py
+++ create_db.py
@@ -10,7 +10,7 @@ from pathlib import Path
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
-    handlers=[logging.FileHandler("_logs/file_to_sqlite.log"), logging.StreamHandler()],
+    handlers=[logging.FileHandler("logs/file_to_sqlite.log"), logging.StreamHandler()],
 )
 
 
